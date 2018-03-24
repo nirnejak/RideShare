20afe5e3c137a5d5ca48440f07fd01c05768a541
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2018-03-24 06:19:12 +0530

    Share Ride, Nearby Rides

--- app.py
+++ app.py
@@ -93,9 +93,9 @@ def login():
 
 		# Get user by either Email or ContactNo
 		if '@' in username:
-			result = cur.execute("SELECT userId, password, userStatus, userType, fname, lname FROM USERS WHERE email = %s",[username])
+			result = cur.execute("SELECT userId, password, userStatus, userType, fname, lname, city FROM USERS WHERE email = %s",[username])
 		else:
-			result = cur.execute("SELECT userId, password, userStatus, userType, fname, lname FROM USERS WHERE contactNo = %s",[username])
+			result = cur.execute("SELECT userId, password, userStatus, userType, fname, lname, city FROM USERS WHERE contactNo = %s",[username])
 
 		if result>0:
 			# Get stored hash
@@ -104,6 +104,7 @@ def login():
 			userId = data['userId']
 			userStatus = data['userStatus']
 			userType = data['userType']
+			city = data['city']
 
 			# Compate Passwords
 			if sha256_crypt.verify(password_candidate, password):
@@ -111,6 +112,7 @@ def login():
 				session['userId'] = userId
 				session['userStatus'] = userStatus
 				session['userType'] = userType
+				session['city'] = city
 				
 				msg = "Welcome {} {}".format(data['fname'],data['lname'])
 				flash(msg,'success')
@@ -147,11 +149,72 @@ def dashboard():
 @app.route('/nearbyRides', methods=['GET','POST'])
 @is_logged_in
 def nearbyRides():
-    return render_template('nearbyRides.html')
+	if request.method == 'POST':
+		RideId = request.form['rideId']
+
+		# Create cursor
+		cur = mysql.connection.cursor()
+
+		# Add User into Database
+		cur.execute("INSERT INTO ShareRequest(RideID, requestUserId) VALUES (%s, %s)", (RideId, session['userId']))
+
+		# Comit to DB
+		mysql.connection.commit()
+
+		# Close connection
+		cur.close()
+
+		
+		flash('Your Request for Ride is sent to the user!','warning')
+		return redirect(url_for('dashboard'))
+	
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Add User into Database
+	result = cur.execute("SELECT * FROM ride WHERE rideDate = DATE(NOW()) and city = %s and rideStatus = %s",(session['city'],"PENDING"))
+
+	rides = cur.fetchall()
+
+	# Comit to DB
+	mysql.connection.commit()
+
+	# Close connection
+	cur.close()
+
+	if result>0:
+		return render_template('nearbyRides.html',rides = rides)
+	else:
+		flash('No Rides in your city!','warning')
+		return redirect(url_for('dashboard'))
 
 @app.route('/rideRequests', methods=['GET','POST'])
 @is_logged_in
 def rideRequests():
+	if request.method == 'POST':
+		pass
+
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Add User into Database
+
+	result = cur.execute("SELECT * FROM ShareRequest")
+
+	rideRequests = cur.fetchall()
+
+	# Comit to DB
+	mysql.connection.commit()
+
+	# Close connection
+	cur.close()
+
+	if result>0:
+		return render_template('rideRequests.html', rideRequests = rideRequests)
+	else:
+		flash('No Requests for Your Ride!','warning')
+		return redirect(url_for('dashboard'))
+
     return render_template('rideRequests.html')
 
 
@@ -160,6 +223,7 @@ def rideRequests():
 def shareRide():
 	if request.method == 'POST':
 		rideDate = request.form['rideDate']
+		rideTime = request.form['rideTime']
 		fromLocation = request.form['fromLocation']
 		toLocation = request.form['toLocation']
 		city = request.form['city']
@@ -169,7 +233,7 @@ def shareRide():
 		cur = mysql.connection.cursor()
 
 		# Add User into Database
-		cur.execute("INSERT INTO Ride(creatorUserId, rideDate, fromLocation, toLocation, city, state) VALUES (%s, %s, %s, %s, %s, %s)", (session['userId'], rideDate, fromLocation, toLocation, city, state))
+		cur.execute("INSERT INTO Ride(creatorUserId, rideDate, rideTime, fromLocation, toLocation, city, state) VALUES (%s, %s, %s, %s, %s, %s,%s)", (session['userId'], rideDate, rideTime, fromLocation, toLocation, city, state))
 
 		# Comit to DB
 		mysql.connection.commit()
---
+++ models.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users(
+	userId INT(11) AUTO_INCREMENT PRIMARY KEY, 
+	fname VARCHAR(100), 
+	lname VARCHAR(100), 
+	contactNo VARCHAR(30) UNIQUE, 
+	alternateContactNo VARCHAR(30), 
+	email VARCHAR(100) UNIQUE, 
+	password VARCHAR(100), 
+	registerDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+	addLine1 VARCHAR(150),
+	addLine2 VARCHAR(150),
+	colony VARCHAR(100),
+	city VARCHAR(100),
+	state VARCHAR(100),
+
+	userStatus VARCHAR(50) DEFAULT "LOGGEDIN",
+	userType VARCHAR(50) DEFAULT "CUSTOMER"
+);
+
+CREATE TABLE Ride(
+	RideId INT(11) AUTO_INCREMENT PRIMARY KEY, 
+	creatorUserId INT(11), # ID of user who sent the request
+	rideDate DATE,
+	rideTime TIME,
+	rideStatus VARCHAR(50) DEFAULT "PENDING",
+
+	fromLocation VARCHAR(100),
+	toLocation VARCHAR(100),
+	state VARCHAR(100),
+	city VARCHAR(100)
+);
+
+
+CREATE TABLE ShareRequest(
+	RequestID INT(11) AUTO_INCREMENT PRIMARY KEY,
+	RideID INT(11),
+	requestUserId INT(11) # ID of user who sent the request
+);
